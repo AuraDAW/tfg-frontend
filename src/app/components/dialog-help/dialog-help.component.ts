0a7e656365822cd7e011e1bf08c827fb3ecc211c
Aura Guerrero <aaronguerreromartinez02@gmail.com>
2025-05-12 11:24:27 +0200

    traducciones dialog de ayuda

--- src/app/components/dialog-help/dialog-help.component.ts
+++ src/app/components/dialog-help/dialog-help.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dialog-help',
-  imports: [MatButtonModule,MatDialogModule],
+  imports: [MatButtonModule,MatDialogModule, TranslateModule],
   templateUrl: './dialog-help.component.html',
   styles: ``
 })
